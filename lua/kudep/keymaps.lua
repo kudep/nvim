3622724672e78b9097dbadba33bb0c0c198b97a1
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2022-10-18 21:59:46 +0300

    dev: upd keymaping

--- lua/kudep/keymaps.lua
+++ lua/kudep/keymaps.lua
@@ -6,7 +6,7 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<space>", "<nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -20,41 +20,42 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<c-h>", "<c-w>h", opts)
+keymap("n", "<c-j>", "<c-w>j", opts)
+keymap("n", "<c-k>", "<c-w>k", opts)
+keymap("n", "<c-l>", "<c-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<c-up>", ":resize -2<cr>", opts)
+keymap("n", "<c-down>", ":resize +2<cr>", opts)
+keymap("n", "<c-left>", ":vertical resize -2<cr>", opts)
+keymap("n", "<c-right>", ":vertical resize +2<cr>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<s-l>", ":bnext<cr>", opts)
+keymap("n", "<s-h>", ":bprevious<cr>", opts)
 keymap("n", "<leader>b", ":ls<cr>", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode 
-keymap("i", "jk", "<ESC>", opts)
+keymap("i", "jk", "<esc>", opts)
 
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+keymap("v", "<leader>ss", '"gy:%s/<c-r>g//gc<left><left><left>', opts)
 
 -- Terminal --
 -- Better terminal navigation
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- keymap("t", "<c-h>", "<c-\\><c-n><c-w>h", term_opts)
+-- keymap("t", "<c-j>", "<c-\\><c-n><c-w>j", term_opts)
+-- keymap("t", "<c-k>", "<c-\\><c-n><c-w>k", term_opts)
+-- keymap("t", "<c-l>", "<c-\\><c-n><c-w>l", term_opts)
 
 -- Diffview -- 
-keymap("n", "<leader>df", ":DiffviewOpen <Up>", opts)
+keymap("n", "<leader>df", ":DiffviewOpen <up>", opts)
 keymap("n", "<leader>dh", "<cmd>DiffviewFileHistory<cr>", opts)
 
 -- Telescope --
@@ -69,4 +70,5 @@ keymap("n", "<leader>tr", "<cmd>Telescope resume<cr>", opts)
 keymap("n", "<leader>ew", "<cmd>NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>er", "<cmd>NvimTreeRefresh<cr>", opts)
 
+-- Terminal --
 keymap("x", "<leader>xc", ":'<,'>ToggleTermSendVisualSelection<cr>", opts)
